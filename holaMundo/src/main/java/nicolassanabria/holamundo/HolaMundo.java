cea1199855dec9535895c537372b91af45d6555f
NicolasSE05 <159843164+NicolasSE05@users.noreply.github.com>
2024-02-12 17:57:44 -0500

    Add files via upload

---
+++ holaMundo/src/main/java/nicolassanabria/holamundo/HolaMundo.java
@@ -0,0 +1,13 @@
+
+package nicolassanabria.holamundo;
+
+/**
+ *
+ * @author Nicolas
+ */
+public class HolaMundo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
